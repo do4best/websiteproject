bf0522eda756490e0c39e51439dcfc80603facad
meer Afzal <do4best@gmail.com>
2024-10-29 19:34:52 +0500

    The project is under the hood and being successful

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import MainNavebar from "@/components/pages/Navebar/mainNavebar";
 import InnerNave from "@/components/pages/Navebar/innerNave";
+import MainTest from "@/components/mywebsite/mainTest";
 
 export default function Home() {
   return (
 
-<InnerNave/>
+<MainTest/>
 
   );
 }
