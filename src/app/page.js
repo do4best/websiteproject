ac94f73d8bee42390b4b695b46f8b72bca00c123
meer Afzal <do4best@gmail.com>
2024-10-30 18:46:18 +0500

    Dark Mode Need to be fixed

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,12 @@ import Image from "next/image";
 import MainNavebar from "@/components/pages/Navebar/mainNavebar";
 import InnerNave from "@/components/pages/Navebar/innerNave";
 import MainTest from "@/components/mywebsite/mainTest";
+import Mainbody from "@/components/pages/body/mainbody";
 
 export default function Home() {
   return (
 
-<MainTest/>
+<Mainbody/>
 
   );
 }
