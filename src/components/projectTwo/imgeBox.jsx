f1d50fb400dbd9c9eeb44c89b114e73f5c4c50af
meer Afzal <do4best@gmail.com>
2024-11-06 18:24:45 +0500

    This project seems to stop here because of advance level of JS is needed and in the example stylecomponent module is used which will conflict the coding style and unlukily i spend 5 days for figuring out but no luck

---
+++ src/components/projectTwo/imgeBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Rmage from './style.module.css';
+
+const ImgeBox=()=> {
+    return (
+        <>
+        <Rmage />
+        </>
+    );
+}
+
+export default ImgeBox;
--- src/components/projectTwo/subProject.jsx
+++ src/components/projectTwo/subProject.jsx
@@ -1,7 +1,14 @@
+'use client'
 import React from 'react';
 import Image from "next/image";
 import logo from '../../../public/logo.svg'
+import pic from '../../../public/image11.jpeg'
+import s from './style.module.css'
+import ImageBox from './imgeBox'
+
+const matrix = [0,1,2,3,4]
 function SubProject(props) {
+
     const message = (name)=>{
         return `${name} is developer`
     }
@@ -11,11 +18,22 @@ function SubProject(props) {
     <div className="bordered  flex cursor-auto fixed left-[400px] w-[1200px] z-1 items-center justify-center p-3">
       <Image className={"h-auto w-max-[760px] w-svw"} src={logo} alt={"This is logo"}/>
     </div>
-    
-    <div className="bordered flex cursor-auto fixed w-svw z-1 bottom-0 justify-between p-4">
-        <h2 className="text-3xl font-bold line-clamp-2 p-4 m-0 after:content=['] text-white block absolute h-1 left-[15px] right-[15px]"></h2>
+    <div className={s.wrapper}>
+   <div className={s['imageContainer']}>
+       {matrix.map((item,index)=> (
+           <div key={index} className={s.Rmage} x={x}/>
+       ))}
+
+   </div>
     </div>
-            </div>
+    <div className="bordered flex cursor-auto fixed w-svw z-1 bottom-0 justify-between p-4">
+        <h2 className="text-3xl font-bold m-0 p-4">June
+            12</h2>
+        <h2 className=" text-center bg-white h-[1px] flex-1 font-bold m-0 p-4 relative after:content-[''] after:block   after:content-absolute after:content-h-[2px] after:content-left-[20px] after:content-right-[150px] after:content-top-[55%] ">World Tour</h2>
+        <h2 className="text-3xl font-bold m-0 p-4">August
+            12</h2>
+    </div>
+</div>
         </>
     );
 }
